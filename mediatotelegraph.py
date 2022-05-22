fc9c82ff63664f217488b911a6bccbc4dc697730
TeLe TiPs - Telegram <89345076+teletips@users.noreply.github.com>
2022-05-22 16:59:17 +0530

    Initial commit

---
+++ mediatotelegraph.py
@@ -0,0 +1,76 @@
+#Copyright ©️ 2022 TeLe TiPs. All Rights Reserved
+#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
+# ##Credits - [MediaToTelegraphLink bot by TeLe TiPs] (https://github.com/teletips/MediaToTelegraphLink-TeLeTiPs)
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from telegraph import upload_file
+import os
+
+teletips=Client(
+    "MediaToTelegraphLink",
+    api_id = 7436880,
+    api_hash = "09e42655b8fd773801f705b01271a011",
+    bot_token = "2124670056:AAF3OVS19vapoKOTY0f6KxaQJvEs6xrA4f0"
+)
+
+@teletips.on_message(filters.command('start') & filters.private)
+async def start(client, message):
+    text = f"""
+Heya {message.from_user.mention},
+I am here to generate Telegraph links for your media files.
+
+Simply send a valid media file directly to this chat.
+Valid file types are 'jpeg', 'jpg', 'png', 'mp4' and 'gif'.
+
+To generate links in **group chats**, add me to your group and send the command <code>/tl</code> as a reply to a valid media file.
+
+🏠 | [Home](https://t.me/teletipsofficialchannel)
+            """
+    await teletips.send_message(message.chat.id, text, disable_web_page_preview=True)
+    
+
+@teletips.on_message(filters.media)
+async def private(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/private/"
+            local_path = await message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return                 
+    except Exception:
+        pass        
+
+@teletips.on_message(filters.command(["tl"]))
+async def get_link_group(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/group/"
+            local_path = await message.reply_to_message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return         
+    except Exception:
+        pass                                           
+
+print("teletips is alive!")
+teletips.run()
+
+#Copyright ©️ 2022 TeLe TiPs. All Rights Reserved
